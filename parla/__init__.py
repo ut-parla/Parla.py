26390274c0e4e7bed507794bf242706e080a783f
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-03-25 17:54:48 -0500

    Force single threaded BLAS.

--- parla/__init__.py
+++ parla/__init__.py
@@ -3,3 +3,9 @@ Parla is a parallel language for orchestrating high-performance array-based prog
 "Orchestration" refers to controlling lower-level operations from a higher-level language.
 In this case, Parla orchestrates low-level array operations and other existing high-performance operations (written in C, C++, or FORTRAN).
 """
+
+# For now, with the prototype, force underlying BLAS libs to run sequentially.
+import os as _os
+
+_os.environ["OMP_NUM_THREADS"] = "1"
+_os.environ["MKL_THREADING_LAYER"] = "SEQUENTIAL"
