5c7294ba5a31549743ea142c2826c33a689fb1db
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-03-25 12:00:31 -0500

    Initial draft of setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "parla",
+      version = "0.1",
+      description = "The parla Python frontend.",
+      packages = ['parla'],
+      )
