6ce93ce7dee29769040540ec1bb1bf96890c0c20
Arthur Peters <amp@singingwizard.org>
2019-07-23 19:02:11 -0500

    Return wrapping version of cupy to make arrays are created on correct dev

--- parla/cuda.py
+++ parla/cuda.py
@@ -1,5 +1,12 @@
+from contextlib import contextmanager
+
+import logging
+from functools import wraps, lru_cache
+
 from . import device
 from .device import *
+
+logger = logging.getLogger(__name__)
 
 try:
     import cupy
@@ -9,14 +16,37 @@ except ImportError as e:
     if all("sphinx" not in f.filename for f in inspect.getouterframes(inspect.currentframe())):
         raise
 
+__all__ = ["gpu"]
+
+
+def _wrap_for_device(ctx, f):
+    @wraps(f)
+    def ff(*args, **kwds):
+        with ctx.context():
+            return f(*args, **kwds)
+    return ff
+
+
+class _DeviceCUPy:
+    def __init__(self, ctx):
+        self._ctx = ctx
+
+    def __getattr__(self, item):
+        v = getattr(cupy, item)
+        if callable(v):
+            return _wrap_for_device(self._ctx, v)
+        return v
 
 class _GPUMemory(Memory):
     @property
+    @lru_cache(None)
     def np(self):
-        return cupy
+        return _DeviceCUPy(self.device)
 
     def __call__(self, target):
+        old = cupy.cuda.Device()
         with self.device.context():
+            logger.info("On %r, moving data to %r, from %r", old, cupy.cuda.Device(), getattr(target, "device", None))
             return cupy.asarray(target)
 
 
@@ -30,8 +60,12 @@ class _GPUDevice(Device):
         with cupy.cuda.Device(self.device_number):
             yield
 
+    @lru_cache(None)
     def memory(self, kind: MemoryKind = None):
         return _GPUMemory(self, kind)
+
+    def __repr__(self):
+        return "<CUDA {}>".format(self.device_number)
 
 
 class _GPUArchitecture(Architecture):
