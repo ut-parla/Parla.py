224648f2052ae434e2fa8f7951a9c2ee85aa20d1
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-09-13 16:43:52 -0500

    Add sequential conjugate gradient example.

---
+++ examples/conjugate_gradient.py
@@ -0,0 +1,83 @@
+# ILU(0) preconditioned conjogate gradient
+# See Algorithm 9.1 of Iterative Methods for Sparse Linear Systems.
+
+import numpy as np
+from scipy import linalg as la
+import scipy.sparse as ss
+import scipy.sparse.linalg as sla
+
+# Note: this example uses CSC format throughout.
+
+# Generate sparse matrix for testing.
+# This is the matrix representation of
+# a 5 point stencil on a 2D grid.
+def discrete_laplacian(n):
+    assert n > 0
+    offset = int(np.sqrt(n))
+    num_edges = n + 2 * (n - 1 - (n - 1) // offset) + 2 * (n - offset)
+    indptr = np.empty(n + 1, np.uint64)
+    indices = np.empty(num_edges, np.uint64)
+    data = np.empty(num_edges)
+    indptr[0] = 0
+    current_indptr = 1
+    current_index = 0
+    for i in range(n):
+        if i >= offset:
+            data[current_index] = -1.
+            indices[current_index] = i - offset
+            current_index += 1
+        if i >= 1 and i % offset:
+            data[current_index] = -1.
+            indices[current_index] = i - 1
+            current_index += 1
+        data[current_index] = 16
+        indices[current_index] = i
+        current_index += 1
+        if i < n - 1 and (i + 1) % offset:
+            data[current_index] = -1.
+            indices[current_index] = i + 1
+            current_index += 1
+        if i < n - offset:
+            data[current_index] = -1.
+            indices[current_index] = i + offset
+            current_index += 1
+        indptr[current_indptr] = current_index
+        current_indptr += 1
+    assert current_indptr == n + 1
+    assert current_index == num_edges
+    return ss.csc_matrix((data, indices, indptr), shape=(n, n))
+
+# Note: Overwrites the initial guess with the solution.
+def cg(A, x_initial, b, tol=1E-8, maxiters=100):
+    x = x_initial
+    r = b - A * x
+    p = r.copy()
+    r_norm2 = np.inner(r, r)
+    for j in range(maxiters):
+        Ap = A * p
+        alpha = r_norm2 / np.inner(Ap, p)
+        x += alpha * p
+        r -= alpha * Ap
+        # Just use inf. norm for convergence (for simplicity)
+        err = np.linalg.norm(r, ord=np.inf)
+        print(err)
+        if err < tol:
+            return x, r
+        z = r.copy()
+        r_norm2_new = np.inner(r, r)
+        p = r + (r_norm2_new / r_norm2) * p
+        r_norm2 = r_norm2_new
+    return x, r
+
+def test_cg():
+    n = 64
+    A = discrete_laplacian(n)
+    b = np.ones(n)
+    x = np.ones(n)
+    x, residual = cg(A, x, b)
+    print(b - A * x)
+    assert(np.linalg.norm(residual, ord=np.inf) < 1E-8)
+
+if __name__ == '__main__':
+    test_cg()
+    print("success")
