a52e05f9e2487553a700b6864dc2cf9681b25fed
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-09-26 14:20:15 -0500

    Save benchmark results to a file.

--- reference_benchmarks/cholesky/build.sh
+++ reference_benchmarks/cholesky/build.sh
@@ -1,8 +1,9 @@
 # Run in a conda environment created like:
-# conda create -n magma_test -c conda-forge magma python=3.7 scikit-build
+# conda create -n magma_test -c conda-forge magma python=3.7 scikit-build cython
 # Be sure to also have a C++17 compatible compiler available.
 rm -rf _skbuild
 python setup.py build_ext --inplace -- -DMagma_LIBRARIES="libmagma.a" -DMagma_INCLUDE_DIR="$CONDA_PREFIX/include" -DMagma_LIB_DIR="$CONDA_PREFIX/lib" -DCUDA_TOOLKIT_ROOT_DIR="$CONDA_PREFIX"
 pushd _skbuild/linux-x86_64-3.7/cmake-build
-python -c "import cholesky_bench; cholesky_bench.bench_cholesky()"
+python -c "import cholesky_bench; cholesky_bench.bench_cholesky()" | tee ../../../benchmark_results.txt
 popd
+rm -rf _skbuild
