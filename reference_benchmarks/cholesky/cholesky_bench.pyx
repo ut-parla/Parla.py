7c128b094c310a25015b16934cd2674dc4e76d08
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-09-26 12:15:28 -0500

    Proper working version of magma potrf call.

--- reference_benchmarks/cholesky/cholesky_bench.pyx
+++ reference_benchmarks/cholesky/cholesky_bench.pyx
@@ -3,17 +3,28 @@
 import numpy as np
 import cupy as cp
 
-cdef extern from "magma.h":
+from libc.stdlib cimport malloc, free
+
+cdef extern from "magma_auxiliary.h" nogil:
+    cppclass magma_queue_t:
+        pass
+    void magma_queue_create(int device, magma_queue_t *queue_ptr)
+    void magma_queue_destroy(magma_queue_t queue_ptr)
+
+cdef extern from "magma_v2.h" nogil:
     ctypedef int magma_int_t
     magma_int_t MAGMA_SUCCESS
     magma_int_t magma_init()
     magma_int_t magma_finalize()
+    void magma_dsetmatrix_1D_col_bcyclic(magma_int_t ngpu, magma_int_t m, magma_int_t n, magma_int_t nb, double *hA, magma_int_t lda, double **dA, magma_int_t ldda, magma_queue_t *queues)
+    void magma_dgetmatrix_1D_col_bcyclic(magma_int_t ngpu, magma_int_t m, magma_int_t n, magma_int_t nb, double **da, magma_int_t ldda, double *hA, magma_int_t lda, magma_queue_t *queues)
     enum magma_uplo_t:
         MagmaUpper
         MagmaLower
         MagmaFull
         MagmaHessenberg
     magma_int_t magma_dpotrf_mgpu(magma_int_t ngpu, magma_uplo_t uplo, magma_int_t n, double **d_IA, magma_int_t ldda, magma_int_t *info)
+    magma_int_t magma_get_dpotrf_nb(magma_int_t n)
 
 # Hack to get number of GPUs from CuPy.
 cdef int get_ngpu():
@@ -29,30 +40,39 @@ cdef int get_ngpu():
 
 def bench_cholesky():
     np.random.seed(0)
-    cdef int n = 16 * 4**2
-    a = np.random.rand(n, n)
-    a = a @ a.T
-    cdef int ngpu = get_ngpu()
-    local_slices = []
-    step = n + (ngpu - 1) // ngpu
-    cdef int i
-    for i in range(ngpu):
-        with cp.cuda.Device(i):
-            #local_slices.append(cp.asarray(a[i::ngpu]))
-            local_slices.append(cp.asarray(a[i*step:(i+1)*step]))
-    cdef size_t[:] d_IA = np.empty(ngpu, np.uintp)
-    for i in range(ngpu):
-        d_IA[i] = local_slices[i].data.ptr
-    cdef int info = 0
+    cdef int n = 16 * 8**2
     cdef magma_int_t status
     status = magma_init()
     assert status == MAGMA_SUCCESS
+    cdef int nb = magma_get_dpotrf_nb(n)
+    data = np.random.rand(n, n)
+    cdef double[:,:] a = data @ data.T
+    a_c = np.copy(a)
+    # Reference result to compare against.
+    res = np.tril(np.linalg.cholesky(a))
+    cdef int ngpu = get_ngpu()
+    local_buffers = []
+    num_blocks = (n + nb - 1) // nb
+    max_blocks_per_device = (num_blocks + ngpu - 1) // ngpu
+    local_buffer_size = n * nb * max_blocks_per_device
+    cdef int i
+    for i in range(ngpu):
+        with cp.cuda.Device(i):
+            local_buffers.append(cp.empty(local_buffer_size, 'd'))
+    cdef size_t[:] d_IA = np.empty(ngpu, np.uintp)
+    for i in range(ngpu):
+        d_IA[i] = local_buffers[i].data.ptr
+    cdef int info = 0
+    cdef magma_queue_t *queues = <magma_queue_t*>malloc(ngpu * sizeof(magma_queue_t))
+    for i in range(ngpu):
+        magma_queue_create(i, &queues[i])
+    magma_dsetmatrix_1D_col_bcyclic(ngpu, n, n, nb, &a[0,0], n, <double**>&d_IA[0], n, &queues[0])
     magma_dpotrf_mgpu(ngpu, MagmaUpper, n, <double**>&d_IA[0], n, &info)
     assert not info
+    magma_dgetmatrix_1D_col_bcyclic(ngpu, n, n, nb, <double**>&d_IA[0], n, &a[0,0], n, &queues[0])
+    for i in range(ngpu):
+        magma_queue_destroy(queues[i])
+    free(<void*>queues)
     status = magma_finalize()
     assert status == MAGMA_SUCCESS
-    res = np.tril(np.linalg.cholesky(a))
-    for i in range(ngpu):
-        #a[i::ngpu] = cp.asnumpy(local_slices[i])
-        a[i*step:(i+1)*step] = cp.asnumpy(local_slices[i])
     assert np.allclose(res, np.tril(a))
