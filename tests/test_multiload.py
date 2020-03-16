5e594be3b604456d4473360bd12da298f400bf6a
Arthur Peters <amp@singingwizard.org>
2020-03-16 16:10:05 -0500

    Add numpy multiload test.

--- tests/test_multiload.py
+++ tests/test_multiload.py
@@ -10,3 +10,19 @@ def test_multiload():
         assert getattr(mod, "_parla_forwarding_module", False)
         # Need forbiddenfruit to make this last one work.
         #assert not hasattr(mod, "unused_id")
+
+def test_multiple_contexts():
+    import timeit
+    multiload_context(1).set_allowed_cpus([0])
+    multiload_context(2).set_allowed_cpus([1,2,3,4,5,6,7])
+    with multiload():
+        import numpy as np
+    def timed_thing():
+        np.int(1)
+        return 0
+        # return timeit.timeit(lambda: np.dot(np.random.rand(2000, 2000), np.random.rand(2000, 2000)), number=1)
+    with multiload_context(1):
+        ctx_1 = timed_thing()
+    with multiload_context(2):
+        ctx_2 = timed_thing()
+    # assert ctx_2 > ctx_1*1.5
