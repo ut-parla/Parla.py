f6ec48a213a158af1824b71e9dfca96430b6c2d6
Arthur Peters <amp@singingwizard.org>
2020-03-21 12:30:19 -0500

    Improve inner product example.
    It now avoids `finish` making it a simpler to explain.

--- examples/inner.py
+++ examples/inner.py
@@ -22,19 +22,19 @@ def main():
         a_part = mapper.partition_tensor(a)
         b_part = mapper.partition_tensor(b)
         # Create array to store partial sums from each logical device
-        partial_sums = np.empty(divisions)
-        # Start a block of tasks that much all complete before leaving the block.
-        async with finish():
-            # For each logical device, perform the local inner product using the numpy multiply operation, @.
-            for i in range(divisions):
-                @spawn(placement=[a_part[i], b_part[i]], memory=storage_size(a_part[i], b_part[i]))
-                def inner_local():
-                    copy(partial_sums[i:i+1], a_part[i] @ b_part[i])
-        # Reduce the partial results (sequentially)
-        res = 0.
-        for i in range(divisions):
-            res += partial_sums[i]
-        return res
+        partial_sums = np.empty(len(a_part))
+        # Define a space of task names for the product tasks
+        P = TaskSpace("P")
+        for i in range(len(a_part)):
+            @spawn(P[i], data=[a_part[i], b_part[i]])
+            def inner_local():
+                # Perform the local inner product using the numpy multiply operation, @.
+                copy(partial_sums[i:i+1], a_part[i] @ b_part[i])
+        @spawn(dependencies=P, data=[partial_sums])
+        def reduce():
+            return np.sum(partial_sums)
+        return await reduce
+
 
     @spawn()
     async def main_task():
