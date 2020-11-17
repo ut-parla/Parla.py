19dc78a823d70a5a9039e7230bc6adf988b33b3e
dialecticDolt <will.ruys@gmail.com>
2020-11-17 15:45:19 -0600

    Disabled unit tests on building library for kokkos example

--- examples/kokkos_multiload_example/build_kokkos.sh
+++ examples/kokkos_multiload_example/build_kokkos.sh
@@ -7,7 +7,7 @@ rm -rf gpu_build
 mkdir gpu_build
 cd gpu_build
 
-CC="$KOKKOS_DIR/bin/nvcc_wrapper" CXX="$KOKKOS_DIR/bin/nvcc_wrapper" ../generate_makefile.bash --cxxflags="-fPIC" --ldflags="-fPIC" --arch=Pascal60 --prefix=${KOKKOS_DIR}/gpu_build/lib/ --with-cuda --with-cuda-options="enable_lambda"
+CC="$KOKKOS_DIR/bin/nvcc_wrapper" CXX="$KOKKOS_DIR/bin/nvcc_wrapper" ../generate_makefile.bash --cxxflags="-fPIC" --ldflags="-fPIC" --arch=Pascal60 --prefix=${KOKKOS_DIR}/gpu_build/lib/ --with-cuda --with-cuda-options="enable_lambda" --disable-tests
 
 make -j 10 kokkoslib
 make install
@@ -19,7 +19,7 @@ rm -rf cpu_build
 mkdir cpu_build
 cd cpu_build
 
-../generate_makefile.bash --cxxflags="-fPIC -fopenmp" --ldflags="-fPIC" --arch=HSW --prefix=${KOKKOS_DIR}/cpu_build/lib/
+../generate_makefile.bash --cxxflags="-fPIC -fopenmp" --ldflags="-fPIC" --arch=HSW --prefix=${KOKKOS_DIR}/cpu_build/lib/ --disable-tests
 
 make -j 10 kokkoslib
 make install
