997bee1f2421f5cd88abfa0667ab03b6a9a136c6
Bozhi YOU <youbozhi@cs.utexas.edu>
2020-12-01 12:38:59 -0600

    Remove remarking _should_run

--- parla/task_runtime.py
+++ parla/task_runtime.py
@@ -636,7 +636,6 @@ class Scheduler(ControllableThread, SchedulerContext):
         self._available_resources = ResourcePool(multiplier=1.0)
         self._unassigned_resources = ResourcePool(multiplier=max_worker_queue_depth*1.0)
         self._worker_threads = [WorkerThread(self, i) for i in range(n_threads)]
-        self._should_run = True
         for t in self._worker_threads:
             t.start()
         self.start()
