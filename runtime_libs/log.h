bb3cbdedb49b7039b6aafe4bf982dc5f1786a823
Ian Henriksen <insertinterestingnamehere@gmail.com>
2021-04-29 15:20:41 -0500

    silence noisy logging in non-debug builds.

--- runtime_libs/log.h
+++ runtime_libs/log.h
@@ -10,7 +10,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+#ifndef NDEBUG
 #define DEBUG(str, ...) fprintf(stderr, "LOG: %s %s:%d " str "\n", __PRETTY_FUNCTION__, __FILE__, __LINE__, ## __VA_ARGS__)
+#else
+#define DEBUG(str, ...)
+#endif
 
 #define CHECK(p, s) if(!(p)) perror(s)
 
