89374960637ed35fe3376cc714626fc33796a05d
Henrique Fingler <hfingler@cs.utexas.edu>
2021-04-29 17:43:38 +0000

    galois does not run within a thread.. nice..

--- examples/galois_multiload_example/README.md
+++ examples/galois_multiload_example/README.md
@@ -8,8 +8,10 @@ something
 
 # Running Galois for testing correctness:
 
-./bfs-cpu -startNode=0 --reportNode=10 --noverify ./rmat15.gr
+./bfs-cpu -startNode=1 --reportNode=10 --noverify ./inputs/rmat15.gr
 
+Distance from 0 to 1-5 is 1
+Distance from 1 to 10 is 2147483646
 
 
 # TODO:
--- examples/galois_multiload_example/galois_bfs_threads.py
+++ examples/galois_multiload_example/galois_bfs_threads.py
@@ -1,32 +1,25 @@
+#!/usr/bin/env python3
 from threading import Thread
-from parla.multiload import multiload, multiload_contexts, mark_module_as_global
-
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="Path to input graph")
 parser.add_argument("vecs", help="# of threads/vecs to run", type=int)
 args = parser.parse_args()
 
-with multiload():
-    import parla_galois.core
-
 from parla_galois.core import py_load_file, py_init_galois
 py_init_galois()
 g = py_load_file(args.input)
 
 def bfs_sssp(i):
-    with multiload_contexts[i]:
-        source = i
-        report = (i+1)*5
-        slot = i
-        g = py_load_file(args.input)
-        parla_galois.core.py_bfs(g, source, slot)
-        print(f"distance from {source} to {report} at slot {slot} is {parla_galois.core.py_distance(g, report, slot)}")
+    source = i
+    report = (i+1)*5
+    slot = i
+    g = py_load_file(args.input)
+    parla_galois.core.py_bfs(g, source, slot)
+    print(f"distance from {source} to {report} at slot {slot} is {parla_galois.core.py_distance(g, report, slot)}")
 
 threads = []
 for i in range(args.vecs):
-    multiload_contexts[i].load_stub_library("numa")
-    #bfs_sssp(i)
     threads.append(Thread(target=bfs_sssp, args=(i,)))
 
 for t in threads: t.start()
