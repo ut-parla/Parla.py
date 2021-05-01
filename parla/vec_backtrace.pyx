04640871c2684b8d7194d64b6661fcfda394e706
Ian Henriksen <insertinterestingnamehere@gmail.com>
2021-05-01 14:58:10 -0500

    Add a Python-facing function for printing out a C backtrace.

--- parla/vec_backtrace.pyx
+++ parla/vec_backtrace.pyx
@@ -2,5 +2,9 @@
 
 cdef extern from "vec_backtrace.h":
     void register_handler()
+    void show_backtrace_with_unwind()
 
 register_handler()
+
+def show_backtrace():
+    show_backtrace_with_unwind()
