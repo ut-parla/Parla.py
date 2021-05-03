f993442a9eeca900ba9440827836fb7af77fabc6
Sean Stephens <sean.e.stephens@gmail.com>
2021-05-03 22:19:56 +0000

    QRF: Updated multithreaded version

--- benchmarks/qr_factorization/qr_multithread.py
+++ benchmarks/qr_factorization/qr_multithread.py
@@ -1,18 +1,12 @@
 # https://arxiv.org/pdf/1301.1071.pdf "Direct TSQR"
 
-import os
-os.environ["OMP_NUM_THREADS"] = "4" # export OMP_NUM_THREADS=4
-
 import sys
-import getopt
-import numpy as np
+import argparse
+import mkl
+#import numpy as np # Do this later so we can set the threadpool size
 import concurrent.futures
+import queue
 import time
-
-ROWS = 240000 # Must be >> COLS
-COLS = 1000
-BLOCK_SIZE = 3000 # Must be >= COLS
-MAX_WORKERS = 6
 
 # Accepts a matrix and returns a list of its blocks
 # block_size rows are grouped together
@@ -36,15 +30,15 @@ def make_blocked(A, block_size):
 def unblock(A):
     return np.concatenate(A)
 
-def tsqr_blocked_multi(A, block_size, workers):
-    if COLS > block_size:
+def tsqr_blocked(A):
+    if NCOLS > BLOCK_SIZE:
         print('Block size must be greater than or equal to the number of columns in the input matrix', file=sys.stderr)
         exit(1)
-    A_blocked, nblocks = make_blocked(A, block_size)
+    A_blocked, nblocks = make_blocked(A, BLOCK_SIZE)
     Q1 = []
     R1 = []
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers = workers) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NGROUPS) as executor:
         # Parallel step 1
         #print('ENTERING FIRST PARALLEL SECTION')
         # Each thread gets a block from A_blocked to run numpy's build-in qr factorization on
@@ -79,7 +73,7 @@ def check_result(A, Q, R):
     
     # Check orthonormal
     Q_check = np.matmul(Q.transpose(), Q)
-    is_ortho_Q = np.allclose(Q_check, np.identity(COLS))
+    is_ortho_Q = np.allclose(Q_check, np.identity(NCOLS))
     
     # Check upper
     is_upper_R = np.allclose(R, np.triu(R))
@@ -87,26 +81,51 @@ def check_result(A, Q, R):
     return is_correct_prod and is_ortho_Q and is_upper_R
 
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[1:], 'r:c:b:w:t:')
-    for opt, arg in opts:
-        if opt == '-r':
-            ROWS = int(arg)
-        elif opt == '-c':
-            COLS = int(arg)
-        elif opt == '-b':
-            BLOCK_SIZE = int(arg)
-        elif opt == '-w':
-            MAX_WORKERS = int(arg)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--rows", help="Number of rows for input matrix; must be >> cols", type=int, default=5000)
+    parser.add_argument("-c", "--cols", help="Number of columns for input matrix", type=int, default=100)
+    parser.add_argument("-b", "--block_size", help="Block size to break up input matrix; must be >= cols", type=int, default=500)
+    parser.add_argument("-i", "--iterations", help="Number of iterations to run experiment", type=int, default=1)
+    parser.add_argument("-w", "--warmup", help="Number of warmup runs to perform before the experiment", type=int, default=0)
+    parser.add_argument("-t", "--threads", help="Number of threads per VEC", default='4')
+    parser.add_argument("-g", "--ngroups", help="Number of thread groups to use (max 11)", type=int, default='4')
+    parser.add_argument("-K", "--check_result", help="Checks final result on CPU", action="store_true")
+    parser.add_argument("--csv", help="Prints stats in csv format", action="store_true")
 
-    print('ROWS=', ROWS, ' COLS=', COLS, ' BLOCK_SIZE=', BLOCK_SIZE, ' MAX_WORKERS=', MAX_WORKERS, sep='')
+    args = parser.parse_args()
+
+    # Set global config variables
+    NROWS = args.rows
+    NCOLS = args.cols
+    BLOCK_SIZE = args.block_size
+    ITERS = args.iterations
+    WARMUP = args.warmup
+    NTHREADS = args.threads
+    NGROUPS = args.ngroups
+    CHECK_RESULT = args.check_result
+    CSV = args.csv
+
+    print('%**********************************************************************************************%\n')
+    print('Config: rows=', NROWS, ' cols=', NCOLS, ' block_size=', BLOCK_SIZE, ' iterations=', ITERS, ' warmup=', WARMUP, \
+        ' threads=', NTHREADS, ' ngroups=', NGROUPS, ' check_result=', CHECK_RESULT, ' csv=', CSV, sep='')
     
-    for i in range(6):
+    mkl.set_num_threads(int(NTHREADS))
+    import numpy as np
+
+    for i in range(WARMUP + ITERS):
         # Original matrix
-        A = np.random.rand(ROWS, COLS)
-        
-        # Multithreaded blocked version
+        np.random.seed(i)
+        A = np.random.rand(NROWS, NCOLS)
+
+        # Multithreaded blocked version with VECs
         start = time.time()
-        Q, R = tsqr_blocked_multi(A, BLOCK_SIZE, MAX_WORKERS)
+        Q, R = tsqr_blocked(A)
         end = time.time()
-        print(f'{end - start}')
-        print(check_result(A, Q, R))
+
+        if (i >= WARMUP):
+            print(end - start)
+
+        if CHECK_RESULT:
+            print(check_result(A, Q, R))
+
+    print('\n%**********************************************************************************************%\n')
