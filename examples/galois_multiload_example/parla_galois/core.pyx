6b9c9ba6ca351dbeaeec356f4155cf28c8008e6e
Henrique Fingler <hfingler@cs.utexas.edu>
2021-05-05 18:45:08 +0000

    make bfs concurrent (nogil

--- examples/galois_multiload_example/parla_galois/core.pyx
+++ examples/galois_multiload_example/parla_galois/core.pyx
@@ -4,19 +4,27 @@ cdef class PyGraph:
     cdef Graph* underlying
 
 def py_distance(unsigned long self, int node, int slot):
-    return read_distance(<Graph*>self, node, slot)
+    g = <Graph*>self
+    cdef int c_node = node
+    cdef int c_slot = slot
+    with nogil:
+        x =  read_distance(g, c_node, c_slot)
+    return x
 
 def py_bfs(unsigned long self, int source, int slot):
-    return bfs(<Graph*>self, source, slot)
-
+    g = <Graph*>self
+    cdef int c_source = source
+    cdef int c_slot = slot
+    with nogil:
+        bfs(g, c_source, c_slot)
 
 def py_load_file(str fn):
-    #r = PyGraph()
     r = <unsigned long>load_file(bytes(fn, "utf-8"))
     return r
 
 def py_init_galois():
-    init_galois()
+    with nogil:
+        init_galois()
 
 def py_delete_galois():
     delete_galois()
