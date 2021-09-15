5ec02f0a092c59176921d79764b35905373e6a79
Hochan Lee <hochan@utexas.edu>
2021-09-15 15:28:42 -0500

    Simplify a script of the third tutorial

--- tutorial/3_devices_and_architectures/devices_and_architectures.py
+++ tutorial/3_devices_and_architectures/devices_and_architectures.py
@@ -7,31 +7,27 @@ from parla.function_decorators import specialized
 
 
 @specialized
-def elemwise_add(x, y):
+def elemwise_add():
+  x = [1, 2, 3]
+  y = [4, 5, 6]
   print(*[x[i]+y[i] for i in range(len(x))], sep=' ')
 
 
 @elemwise_add.variant(gpu)
-def elemwise_add_gpu(x, y):
+def elemwise_add_gpu():
+  x = cupy.array([1, 2, 3])
+  y = cupy.array([4, 5, 6])
   cupy_elemwise_add = cupy.ElementwiseKernel("int64 x, int64 y", "int64 z", "z = x + y")
   print(*cupy_elemwise_add(x, y), sep=' ')
 
 
 def main():
-  x_gpu = cupy.array([1, 2, 3])
-  y_gpu = cupy.array([4, 5, 6])
-  x_cpu = [1, 2, 3]
-  y_cpu = [4, 5, 6]
-
   arch_modes = [cpu, gpu(0)]
 
   for arch_mode in arch_modes:
     @spawn(placement = arch_mode)
     def elemwise_add_task():
-      if (arch_mode == cpu):
-        elemwise_add(x_cpu, y_cpu)
-      else:
-        elemwise_add(x_gpu, y_gpu)
+      elemwise_add()
 
 
 if __name__ == "__main__":
