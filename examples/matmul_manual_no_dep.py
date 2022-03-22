7907725557d429b9de4468a325547a3f1bf3d44c
dialecticDolt <will.ruys@gmail.com>
2022-03-22 18:43:46 -0500

    Fix stride

--- examples/matmul_manual_no_dep.py
+++ examples/matmul_manual_no_dep.py
@@ -28,11 +28,12 @@ def main():
         # n is chosen so that all three can be
         # stored within the memory of a single GPU
         # so that strong scaling numbers make sense.
-        n = 24000
+        n = 12000
 
         blocks = ngpus
         block_size = n // ngpus
-        ordr = 'F'
+        h_ordr = 'C'
+        d_ordr = 'F'
         print("BlockSize: ", block_size, "GPUS: ", ngpus)
 
         # Overdecomposing doesn't actually seem to help in this case
@@ -42,8 +43,8 @@ def main():
         # testing later.
 
         np.random.seed(0)
-        a_cpu = np.random.rand(n, n).astype(dtype=np.float32, order=ordr)
-        b_cpu = np.random.rand(n, n).astype(dtype=np.float32, order=ordr)
+        a_cpu = np.random.rand(n, n).astype(dtype=np.float32, order=h_ordr)
+        b_cpu = np.random.rand(n, n).astype(dtype=np.float32, order=h_ordr)
 
         print("Finished Data Allocation", flush=True)
         # Partition the two arrays and set up the
@@ -65,8 +66,10 @@ def main():
         for i in range(blocks):
             if distribute:
                 with cp.cuda.Device(i):
-                    a_part.append(cp.asarray(a_cpu[i * block_size : (i + 1) * block_size]))
-                    b_part.append(cp.asarray(b_cpu[i * block_size : (i + 1) * block_size]))
+                    a_part.append(cp.asarray(a_cpu[i * block_size : (i + 1) * block_size], order=d_ordr))
+                    b_part.append(cp.asarray(b_cpu[i * block_size : (i + 1) * block_size], order=d_ordr))
+                    cp.cuda.stream.get_current_stream().synchronize()
+
             else:
                 a_part.append(a_cpu[i * block_size : (i + 1) * block_size])
                 b_part.append(b_cpu[i * block_size : (i + 1) * block_size])
@@ -75,7 +78,7 @@ def main():
         for i in range(blocks):
             c_part.append(list())
             for j in range(blocks):
-                c_part[i].append(np.empty((0, 0), dtype=np.float32, order=ordr))
+                c_part[i].append(np.empty((0, 0), dtype=np.float32, order=h_ordr))
 
         #print(len(c_part), len(c_part[0]), c_part[0][0].shape)
 
@@ -84,9 +87,11 @@ def main():
             #reset cblocks to None
             for i in range(blocks):
                 for j in range(blocks):
-                    c_part[i][j] = np.empty((0, 0), dtype=np.float32, order=ordr)
+                    c_part[i][j] = np.empty((0, 0), dtype=np.float32, order=h_ordr)
 
             matmul = TaskSpace("matmul")
+            cp.sin(cp.asarray([2]))
+            cp.cuda.stream.get_current_stream().synchronize()
             start = time.perf_counter()
             for i in range(blocks):
                 for j in range(blocks):
