6b21a7ecd6164ce74d16259d3566bd21030428d9
dialecticDOlt <will.ruys@gmail.com>
2022-07-23 18:40:57 -0400

    Starting to add launcher framework

--- examples/artifact_launcher.py
+++ examples/artifact_launcher.py
@@ -1,49 +1,60 @@
 import os
 import sys
+import argparse
+import pexpect as pe
+
+#######
+# Define functions to gather each result (per figure, per app)
+#######
 
 #Figure 10: Cholesky
-def run_cholesky_28():
-    pass
+def run_cholesky_28(gpu_list):
+
+    for n_gpus in gpu_list:
+        command = f"python test_script.py -gpus {n_gpus}"
+        output = pe.run(command, timeout=100, withexitstatus=True)
+        assert(output[1] == 0)
+
 
 #Figure 13: Parla Cholesky (CPU)
 def run_cholesky_20_host():
     pass
 
 #Figure 13: Parla Cholesky (GPU)
-def run_cholesky_20_gpu():
+def run_cholesky_20_gpu(gpu_list):
     pass
 
 #Figure 13: Dask Cholesky (CPU)
-def run_dask_cholesky_20_host():
+def run_dask_cholesky_20_host(cores_list):
     pass
 
 #Figure 13: Dask Cholesky (GPU)
-def run_dask_cholesky_20_gpu():
+def run_dask_cholesky_20_gpu(gpu_list):
     pass
 
 #Figure 10: Jacobi
-def run_jacobi():
+def run_jacobi(gpu_list):
     pass
 
 
 #Figure 10: Matmul
-def run_matmul():
+def run_matmul(gpu_list):
     pass
 
 #Figure 10: BLR
-def run_blr():
+def run_blr(gpu_list):
     pass
 
 #Figure 10: NBody
-def run_nbody():
+def run_nbody(gpu_list):
     pass
 
 #Figure 10: Synthetic Reduction
-def run_reduction():
+def run_reduction(gpu_list):
     pass
 
 #Figure 10: Synthetic Independent
-def run_independent():
+def run_independent(gpu_list):
     pass
 
 #Figure 10: Synthetic Serial
@@ -62,10 +73,17 @@ def run_prefetching_test():
 def run_GIL_test():
     pass
 
+test = [run_cholesky_20_host]
 figure_10 = [run_cholesky_28, run_jacobi, run_matmul, run_blr, run_nbody, run_reduction, run_independent, run_serial]
 figure_13 = [run_cholesky_20_host, run_cholesky_20_gpu, run_dask_cholesky_20_host, run_dask_cholesky_20_gpu]
 figure_15 = [run_batched_cholesky]
 figure_12 = [run_prefetching_test]
+
+
+for f in test:
+    f()
+
+
 
 
 
