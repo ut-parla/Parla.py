ebfd615b729ac9d539435b314141df7e59934496
dialecticDolt <will.ruys@gmail.com>
2022-08-02 11:56:42 -0500

    Fix artifact paths

--- examples/source.sh
+++ examples/source.sh
@@ -1,7 +1,10 @@
 export PARLA_ROOT=$(pwd)
 export MAGMA_ROOT=$PARLA_ROOT/artifact/magma
-export CUBLASMG_ROOT=$PARLA_ROOT/artifact/cublasmg
-export CUDAMG_ROOT=$PARLA_ROOT/artifact/cudalibmg
+export CUBLASMG_ROOT=$PARLA_ROOT/artifact/cublasmg/cublasmg
+export CUDAMG_ROOT=$PARLA_ROOT/artifact/cublasmg/cudalibmg
+
+#Please set your CUDA_ROOT
+#export CUDA_ROOT=<your cuda installation here>
 
 #Optional: Set PYTHONPATH
 #export PYTHONPATH = $PARLA_ROOT:$PYTHONPATH
