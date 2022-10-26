af96a1e572c735dd9ba28bd89c4d6302b308af1b
dialecticDolt <will.ruys@gmail.com>
2022-10-26 10:07:37 -0500

    Add cpu matmul example

---
+++ examples/matmul/matmul_cpu.py
@@ -0,0 +1,151 @@
+"""
+Multi-device matrix multiplication using parla with cupy as the kernel engine.
+
+"""
+import sys
+import time
+import os
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+#Size of matrix
+parser.add_argument('-n', type=int, default=32000, help='Size of matrix')
+#How many trials to run
+parser.add_argument('-trials', type=int, default=1, help='Number of trials to run')
+#Are the placement fixed by the user or determed by the scheduler?
+parser.add_argument('-fixed', default=0, type=int, help="User Mapping (1) or Scheduler Mapping (0)")
+#Number of devices to use
+parser.add_argument('-nthreads', type=int, default=1, help="Number of workers to use")
+#Number of devices to use
+parser.add_argument('-ithreads', type=int, default=1, help="Internal Threads")
+args = parser.parse_args()
+
+import mkl
+load = 1.0/args.nthreads
+
+mkl.set_num_threads(args.ithreads)
+os.environ["NUMEXPR_NUM_THREADS"] = str(args.ithreads)
+os.environ["OMP_NUM_THREADS"] = str(args.ithreads)
+os.environ["OPENBLAS_NUM_THREADS"] = str(args.ithreads)
+os.environ["VECLIB_MAXIMUM_THREADS"] = str(args.ithreads)
+
+import numpy as np
+
+from parla import Parla, get_all_devices
+from parla.array import copy, clone_here
+from parla.cpu import cpu
+from parla.cuda import gpu
+from parla.function_decorators import specialized
+from parla.ldevice import LDeviceSequenceBlocked
+from parla.tasks import spawn, TaskSpace, CompletedTaskSpace, reserve_persistent_memory
+from parla.parray import asarray_batch
+
+
+
+def main():
+
+    @spawn(placement=cpu)
+    async def main_task():
+        repetitions = args.trials
+
+        # set up two n x n arrays to multiply together.
+        # n is chosen so that all three can be
+        # stored within the memory of a single GPU
+        # so that strong scaling numbers make sense.
+        n = args.n
+
+        blocks = args.nthreads
+        block_size = n // args.nthreads
+        h_ordr = 'C'
+        d_ordr = 'F'
+        print("BlockSize: ", block_size, "Workers: ", blocks)
+
+        # Overdecomposing doesn't actually seem to help in this case
+        # with the current parla runtime. This may be related to
+        # some weirdness within the scheduler though, so
+        # we can leave the code for blocks in-place for further
+        # testing later.
+
+        np.random.seed(0)
+        a_cpu = np.random.rand(n, n).astype(dtype=np.float32, order=h_ordr)
+        b_cpu = np.random.rand(n, n).astype(dtype=np.float32, order=h_ordr)
+
+        print("Finished Data Allocation", flush=True)
+        # Partition the two arrays and set up the
+        # partitioned array where the result will be stored.
+        # This could also be done using a parla mapper object.
+
+        a_part = []
+        b_part = []
+        c_part = []
+
+        distribute=True
+        fixed_placement=args.fixed
+        verbose=False
+        sync = False
+
+        time_list = list()
+
+        # Start all operans from CPU memory.
+        for i in range(blocks):
+            a_part.append(a_cpu[i * block_size : (i + 1) * block_size])
+            b_part.append(b_cpu[i * block_size : (i + 1) * block_size])
+
+
+        for i in range(blocks):
+            c_part.append(list())
+            for j in range(blocks):
+                c_part[i].append(np.empty((0, 0), dtype=np.float32, order=h_ordr))
+
+        for repetition in range(repetitions):
+
+            #reset cblocks to None
+            for i in range(blocks):
+                for j in range(blocks):
+                    c_part[i][j] = np.empty((0, 0), dtype=np.float32, order=h_ordr)
+
+            matmul = TaskSpace("matmul")
+
+            start = time.perf_counter()
+            for i in range(blocks):
+                for j in range(blocks):
+                    a_block = a_part[i]
+                    b_block = b_part[j]
+                    c_block = c_part[i][j]
+
+                    memsize = (block_size**2 + block_size*n*2)*4
+
+                    @spawn(matmul[i, j], placement = cpu, memory=memsize, vcus=load)
+                    def matmul_task():
+
+                        a = clone_here(a_block)
+                        b = clone_here(b_block)
+                        c = clone_here(c_block)
+
+                        if verbose:
+                            print(f"+({i}, {j}): ", a.shape, b.shape, c.shape, flush=True)
+
+                        local_start = time.perf_counter()
+                        c = a @ b.T
+
+                        local_end = time.perf_counter()
+
+                        c_part[i][j] = c
+
+                        if verbose:
+                            print(f"-({i}, {j}): ", a.shape, b.shape, c.shape, " | Elapsed: ", local_end-local_start, flush=True)
+
+            await matmul
+            stop = time.perf_counter()
+            print(f"Iteration {repetition} | Time: ", stop - start, flush=True)
+            time_list.append(stop-start)
+
+        mean = np.mean(np.array(time_list))
+        median = np.median(np.array(time_list))
+
+        print(f"Execution:: Average = {mean} | Median = {median}", flush=True)
+if __name__ == "__main__":
+    with Parla():
+        main()
